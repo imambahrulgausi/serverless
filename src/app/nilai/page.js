fe98c4183f73f2dc6514e6110a8270ad6e902b2a
imambahrulgausi <imambahrulgausi@gmail.com>
2024-12-31 14:28:09 +0700

    perubahan hal nilai

--- src/app/nilai/page.js
+++ src/app/nilai/page.js
@@ -2,7 +2,7 @@ export default function Nilai() {
     return (
         <>
             <h1 className="text-md font-semibold">
-                Halaman Nilai
+                Halaman Nilai update
             </h1>
         </>
     )
